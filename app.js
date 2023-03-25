1f732b7a4cc4e61e3d798efe38ba796716dca4c3
fjorg58 <flavio.jorge.email@gmail.com>
2023-03-25 17:38:10 -0400

    Made a few changes, hope you don't mind

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const allImages = document.getElementsByTagName('img');
 
+//added comment here
 for (let img of allImages) {
     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
 }
